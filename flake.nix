0e008d58820ffbbf3a2e4f40e9e6d30954eed944
menixator <3684951+menixator@users.noreply.github.com>
2024-09-02 13:32:19 +0500

    build: attempt generating docs

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# https://discourse.nixos.org/t/documentation-for-custom-nixos-options/20320/5
 {
   inputs = { };
 
@@ -9,5 +10,19 @@
     {
       nixosModules.default = lancache;
       nixosModules.lancache = lancache;
+      documentation =
+        let
+          # evaluate our options
+          nixpkgs = builtins.getFlake ("github:nixos/nixpkgs/e69fc881bb11a8280c2cdf94c1aaf391ef3e3677");
+          lib = nixpkgs.lib;
+
+          eval = nixpkgs.lib.evalModules { modules = [ ./lancache.nix ]; };
+          # generate our docs
+          optionsDoc = nixpkgs.legacyPackages.x86_64-linux.nixosOptionsDoc { inherit (eval) options; };
+        in
+        # create a derivation for capturing the markdown output
+        nixpkgs.legacyPackages.x86_64-linux.runCommand "options-doc.md" { } ''
+          cat ${optionsDoc.optionsCommonMark} >> $out
+        '';
     };
 }
